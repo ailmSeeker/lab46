e8f84fd26cddb8674fdcf2ec22befc70eac035c7
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-25 06:39:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.getElementById("done");
+
+document.getElementById('create-task-form').addEventListener('submit', function(evt){
+    evt.preventDefault();
+    let data = document.querySelector("#new-task-description").value
+    let list = document.getElementById('list') 
+     
+    const li = document.createElement("li");
+    li.innerText = data;
+    list.appendChild(li);
+})    
+
